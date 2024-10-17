9dbffb4de5a6b065fa6e51bc51d9f73e94a71b90
Sasha <competentheisenberg@gmail.com>
2024-10-17 14:38:53 +0300

    First Test

---
+++ src/mpage.js
@@ -0,0 +1,15 @@
+// src/MyPage.js
+import React from 'react';
+import './css/mpage.css';
+
+function MyPage() {
+  return (
+    <div>
+      <h1>Моя сторінка</h1>
+      <p>Вітаю!</p>
+      <button className="button">Click Me</button>
+    </div>
+  );
+}
+
+export default MyPage;
