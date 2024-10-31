44e3e52f4149c40fdb2ea937b57da1c34208e5b5
KosovilkaOleksandr <kosovilka.oleksandr@student.uzhnu.edu.ua>
2024-10-31 02:04:33 +0200

    Fixed Full Code

--- src/components/mpage.js
+++ src/components/mpage.js
@@ -2,12 +2,12 @@ import React from "react";
 import styles from "./css/mpage.module.css";
 import Button1 from "../components_elem/Button1";
 import Forminp from "../components_elem/Forminp";
-
+//test
 function MyPage() {
   return (
     <div className={styles.container}>
       <form>
-        <h1>Create an account</h1>
+        <h1>Create account</h1>
         <p>
           By creating an account, you agree to this apps {""}
           <button
@@ -37,30 +37,11 @@ function MyPage() {
         </div>
         <div className={styles.textbox}>
           <label htmlFor="surname">Surname</label>
-          <input
-            type="text"
-            id="surname"
-            name="surname"
-            placeholder="Enter your surname"
-            required
-          />
           <Forminp placeholder="Surname" type="text"></Forminp>
         </div>
         <div className={styles.textbox}>
           <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Enter your password"
-            required
-          />
         </div>
-        <button type="submit" className={styles.submitButton}>
-          Sign up
-        </button>
-        <p className={styles.loginText} />
-        Have an account? <a href="/login">Log in</a>
         <Forminp placeholder="Password" type="password"></Forminp>
         <Button1>Sign Up</Button1>
         <p className={styles.loginText}>
