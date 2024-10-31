22d8ca819accef5a6e8836adfe89b532c8ba8844
KosovilkaOleksandr <kosovilka.oleksandr@student.uzhnu.edu.ua>
2024-10-31 02:04:33 +0200

    Fixed Full Code

--- src/components/mpage.js
+++ src/components/mpage.js
@@ -1,13 +1,56 @@
-// src/MyPage.js
-import React from 'react';
-import './css/mpage.css';
-
+import React from "react";
+import styles from "./css/mpage.module.css";
+import Button1 from "../components_elem/Button1";
+import Forminp from "../components_elem/Forminp";
+//test
 function MyPage() {
   return (
-    <div>
-      <h1>Моя сторінка:</h1>
-      <p>Вітаю вас!</p>
-      <button className="button">Click Me</button>
+    <div className={styles.container}>
+      <form>
+        <h1>Create account</h1>
+        <p>
+          By creating an account, you agree to this apps {""}
+          <button
+            type="button"
+            className={styles.linkButton}
+            onClick={() => alert("Terms clicked!")}
+          >
+            Terms
+          </button>{" "}
+          and{" "}
+          <button
+            type="button"
+            className={styles.linkButton}
+            onClick={() => alert("Privacy Policy clicked!")}
+          >
+            Privacy Policy
+          </button>
+          .
+        </p>
+        <div className={styles.textbox}>
+          <label htmlFor="email">Email address</label>
+          <Forminp placeholder="Email" type="text"></Forminp>
+        </div>
+        <div className={styles.textbox}>
+          <label htmlFor="name">Name</label>
+          <Forminp placeholder="Name" type="text"></Forminp>
+        </div>
+        <div className={styles.textbox}>
+          <label htmlFor="surname">Surname</label>
+          <Forminp placeholder="Surname" type="text"></Forminp>
+        </div>
+        <div className={styles.textbox}>
+          <label htmlFor="password">Password</label>
+        </div>
+        <Forminp placeholder="Password" type="password"></Forminp>
+        <Button1>Sign Up</Button1>
+        <p className={styles.loginText}>
+          Have an account?{" "}
+          <a href="/login" className={styles.linkButton}>
+            Log in
+          </a>
+        </p>
+      </form>
     </div>
   );
 }
