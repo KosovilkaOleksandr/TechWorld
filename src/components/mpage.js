5c0be272407c32969bbcb4e9b08e6f9595e5e252
KosovilkaOleksandr <kosovilka.oleksandr@student.uzhnu.edu.ua>
2024-10-31 07:08:46 +0200

    TESTS

--- src/components/mpage.js
+++ src/components/mpage.js
@@ -1,7 +1,9 @@
 // src/MyPage.js
 import React from "react";
-import "./css/mpage.css";
-
+import styles from "./css/mpage.module.css";
+import Button1 from "../components_elem/Button1";
+import Forminp from "../components_elem/Forminp";
+//testing
 function MyPage() {
   return (
     <div>
